1dc8819cfa21e1ac51a88016499bd6ed8e050b6e
rinpodevil <96582600+rinpodevil@users.noreply.github.com>
2021-12-27 15:08:23 +0530

    Add files via upload

--- waterjug.py
+++ waterjug.py
@@ -1,19 +1,33 @@
 def waterjug():
     print("water jug problem")
-    xc=int(input("enter the x gallon capacity"))
-    yc=int(input("enter the y gallon capacity"))
-    x=int(input("enter the x value"))
-    y=int(input("enter the y value"))
+    xc=int(input("enter the x gallon capacity:"))
+    yc=int(input("enter the y gallon capacity:"))
+    x=int(input("enter the x value:"))
+    y=int(input("enter the y value:"))
+    print("enter the x and y gosl state")
+    xa=int(input())
+    xb=int(input())
+
     while True:
         rno=int(input("enter the rule no"))
         if rno==1:
             if x<xc:
                 x=xc
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
+
             else:
                 print("out of capacity")
         elif rno==2:
             if y<yc:
                 y=yc
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
+                
             else:
                 print("out of capacity")
         elif rno==3:
@@ -21,6 +35,10 @@ def waterjug():
                 print("enter capacity")
                 d=int(input())
                 x=x-d 
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==4:
@@ -28,42 +46,70 @@ def waterjug():
                 print("enter capacity")
                 d=int(input())
                 y=y-d 
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==5:
             if x>0:
                 x=0
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==6:
             if y>0:
                 y=0
+                print("x",x,"y",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==7:
             if x+y>=xc and y>0:
                 x,y=xc,y-(xc-x)
+                print("x:",x,"y:",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==8:
             if x+y>=yc and x>0:
                 x,y=x-(yc-y),yc
+                print("x:",x,"y:",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==9:
-            if x+y<=xc and y>0:
+            if x+y<=xc and y>0: 
                 x,y=x+y,0
+                print("x:",x,"y:",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         elif rno==10:
             if x+y<=yc and x>0:
                 x,y=0,x+y
+                print("x:",x,"y:",y)
+                if x==xa and y==xb:
+                    print("goal state")
+                    break
             else:
                 print("not happening")
         
         else:
             break;
-    print("x:",x,"y:",y)
+    
 waterjug()
             
 
